60a6aae893707230b25c785946a9d4fe6024aef7
christophebe <christophe.lombart@gmail.com>
2015-09-11 07:24:06 +0200

    add dependency

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe('Domain Check', function() {
         it.only('Check if a domain is not available', function(done) {
             this.timeout(40000);
 
-            checkDomain({domain : "pret-personnel-comparatif.com"}, function(error, result) {
+            checkDomain({domain : "google.com"}, function(error, result) {
                 console.log(result);
                 done();
             });
